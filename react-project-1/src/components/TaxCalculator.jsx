eff0b98c80b68f8316a5b2e5cc28f85e7805e058
kaden-d <kaden-d@hotmail.com>
2024-02-01 14:07:53 -0700

    converted raw js to jsx

--- react-project-1/src/components/TaxCalculator.jsx
+++ react-project-1/src/components/TaxCalculator.jsx
@@ -2,65 +2,66 @@ import React, { Component } from 'react';
 
 class TaxCalculator extends Component {
     state = { 
-        income: this.props
-     } 
-
+        message : "Please enter your yearly income in the field above."
+    }
+    handleChange = (event) => {
+        this.setState({ income: event.target.value });
+    }
     
     //this function takes in two variables from the HTML in the form of the input id and output id.
-    processEntry = (income, output) => 
+    processEntry = (income) => 
     {
-    //simplifying the document.getElementById() method as a $.
-    let $ = function (id) { return document.getElementById(id) };
-    //storing the income variable by pulling the value from the input's source field and declaring the message variable.
-    let income = $(input).value;
-    let message;
-    //declaring a Dollar object for javascript's built-in for number formatting as a currency.
-    let Dollar = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' });
-    //"error" handling in the form of checking that the income value is a float and above 0.
-    if (income > 0)
-    {
-        message = 'You will owe: ' + Dollar.format(calculateTax(parseFloat(income)));
-    }
-    else if (isNaN(income))
-    {
-        message = 'Please enter a number for your income.';
-    }
-    else
-    {
-        message = 'Please enter a number greater than zero.';
-    }
-    //changing the output text field to match the message variable.
-    $(output).textContent = message;
-};
-    //this function serves to take in a numerical value and calculate the tax owed, separating at each bracket.
- calculateTax = income =>
-{
-    //declaring two constant arrays, the BRKTS (or brackets) and BRKTS_TAXES (or the taxes for each bracket)
-    const BRKTS = [9225, 37450, 90750, 189300, 411500, 413200, 0];
-    const BRKT_TAXES = [0.10, 0.15, 0.25, 0.28, 0.33, 0.35, 0.396];
-    //declaring the amountOwed and remainder variables for use later in the function.
-    let amountOwed = 0, remainder = 0;
-    /*
-    the for loop iterates through the BRKTS array and pulls apart the income,
-    calculates the tax for each bracket and then adds that tax to the amountOwed.
-    */
-    for (let i in BRKTS)
-    {
-        remainder = income - BRKTS[i]
-        if (remainder < 0 || remainder == income)
+        let message;
+        //declaring a Dollar object for javascript's built-in for number formatting as a currency.
+        let Dollar = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' });
+        //"error" handling in the form of checking that the income value is a float and above 0.
+        if (income > 0)
         {
-            amountOwed += (income * BRKT_TAXES[i]);
-            break;
+            message = 'You will owe: ' + Dollar.format(this.calculateTax(parseFloat(income)));
+        }
+        else if (isNaN(income))
+        {
+            message = 'Please enter a number for your income.';
         }
         else
         {
-            amountOwed += ((income - remainder) * BRKT_TAXES[i]);
-            income = remainder;
+            message = 'Please enter a number greater than zero.';
         }
-    }
-    //returns the amount of tax owed.
-    return amountOwed;
-};
+        //changing the state message to the local message output variable.
+        this.setState({ message });
+    };
+
+
+
+    //this function serves to take in a numerical value and calculate the tax owed, separating at each bracket.
+    calculateTax = income =>
+    {
+        //declaring two constant arrays, the BRKTS (or brackets) and BRKTS_TAXES (or the taxes for each bracket)
+        const BRKTS = [9225, 37450, 90750, 189300, 411500, 413200, 0];
+        const BRKT_TAXES = [0.10, 0.15, 0.25, 0.28, 0.33, 0.35, 0.396];
+        //declaring the amountOwed and remainder variables for use later in the function.
+        let amountOwed = 0, remainder = 0;
+        /*
+        the for loop iterates through the BRKTS array and pulls apart the income,
+        calculates the tax for each bracket and then adds that tax to the amountOwed.
+        */
+        for (let i in BRKTS)
+        {
+            remainder = income - BRKTS[i]
+            if (remainder < 0 || remainder == income)
+            {
+                amountOwed += (income * BRKT_TAXES[i]);
+                break;
+            }
+            else
+            {
+                amountOwed += ((income - remainder) * BRKT_TAXES[i]);
+                income = remainder;
+            }
+        }
+        //returns the amount of tax owed.
+        return amountOwed;
+    };
 
 
     render() { 
@@ -70,7 +71,7 @@ class TaxCalculator extends Component {
             <div className="bubble">
                 <div className="row">
                     <label>Yearly income: </label><br/>
-                    <input type="text" id="income" name="income"></input>
+                    <input type="text" id="income" name="income" onChange={this.handleChange}></input>
                 </div>
 
                 <div className="row">
@@ -78,12 +79,12 @@ class TaxCalculator extends Component {
                         id="processEntryBtn" 
                         className="processEntryBtn"
                         type="button"
-                        onClick={() => processEntry(income, owed)}
+                        onClick={() => this.processEntry(this.state.income)}
                         >Process
                     </button>
                 </div>
                 <div className="row">
-                    <p id="owed">Please enter your yearly income in the field above.</p>
+                    <p id="owed">{ this.state.message }</p>
                 </div>
             </div>
         </div>);

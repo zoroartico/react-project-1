c89dbd9c8235939e8c225036f64cd6ea75b4f51e
kaden-d <kaden-d@hotmail.com>
2024-02-01 10:09:58 -0700

    Added TaxCalulator.jsx and added template

---
+++ react-project-1/src/components/TaxCalculator.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class TaxCalculator extends Component {
+    state = {  } 
+    render() { 
+        return (<div>
+            
+        </div>);
+    }
+}
+ 
+export default TaxCalculator;
--- react-project-1/src/index.js
+++ react-project-1/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import TaxCalculator from './components/TaxCalculator';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <TaxCalculator />
   </React.StrictMode>
 );
 

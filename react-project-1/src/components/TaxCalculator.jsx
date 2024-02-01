5e222e73946150c80a6d8286f21d3605f5dbe6da
kaden-d <kaden-d@hotmail.com>
2024-02-01 10:35:59 -0700

    Extracated your html repo to jsx

--- react-project-1/src/components/TaxCalculator.jsx
+++ react-project-1/src/components/TaxCalculator.jsx
@@ -2,12 +2,34 @@ import React, { Component } from 'react';
 
 class TaxCalculator extends Component {
     state = {  } 
+
+
     render() { 
         return (
         <div>
-            
+            <h1>Tax Calculator</h1>
+            <div className="bubble">
+                <div className="row">
+                    <label>Yearly income: </label><br/>
+                    <input type="text" id="income" name="income"></input>
+                </div>
+
+                <div className="row">
+                    <button 
+                        id="processEntryBtn" 
+                        className="processEntryBtn"
+                        type="button"
+                        onClick="processEntry('income', 'owed')"
+                        >Process
+                    </button>
+                </div>
+                <div className="row">
+                    <p id="owed">Please enter your yearly income in the field above.</p>
+                </div>
+            </div>
         </div>);
     }
+
 }
  
 export default TaxCalculator;
